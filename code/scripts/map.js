d1ecf4379f8c65b3c965cdc57cb04d0a15cae773
Jakob <jakob-151@web.de>
2021-04-19 19:51:17 +0200

    basic GUI and Button interaction

--- code/scripts/map.js
+++ code/scripts/map.js
@@ -1,10 +1,10 @@
 var mymap = L.map(document.getElementById('mapid')).setView([50.80344938458088, 8.766699833424578], 11);
 
 // Current guessing marker
-var curr_marker
+var curr_marker = {}
 
 // Real Coordinate of picture
-var pos_gaol = [50.79866043515019, 8.762314364220718]
+var pos_goal = [50.79866043515019, 8.762314364220718];
 
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -23,7 +23,21 @@ function onMapClick(e) {
     }
     curr_marker = L.marker(e.latlng);
     mymap.addLayer(curr_marker);
-    console.log(coord_dist(pos_gaol, e.latlng))
 }
 
 mymap.on('click', onMapClick);
+
+function lock_guess() {
+    let dist = coord_dist(pos_goal, curr_marker._latlng);
+    let result = "";
+    if (dist >= 1) {
+        result = `${dist.toFixed(2)}km`
+    } else {
+        result = `${(dist*1000).toFixed(1)}m`
+    }
+    modal = document.getElementById('result_screen');
+// When the user clicks the button, open the modal 
+    modal.style.display = "block";
+
+    document.getElementById('test').innerHTML = `Marker ist ${result} vom Ziel entfernt`
+}
