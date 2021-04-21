090ad567c6be97d610d9a879d7d5247d219035ae
Jakob <jakob-151@web.de>
2021-04-21 21:47:42 +0200

    Swiched to different pano viewer

--- code/scripts/pano.js
+++ code/scripts/pano.js
@@ -1,22 +1,19 @@
 var container
-var viewer
+var PanoViewer = eg.view360.PanoViewer;
 var last_pano
+var viewer
 
 function display_pano(id) {
     if (viewer == undefined) {
         container = document.querySelector('#pano')
-        viewer = new PANOLENS.Viewer({ container: container, controlBar: false });
+        viewer = new PanoViewer(container, {projectionType: 'eg.view360.PanoViewer.PROJECTION_TYPE.EQUIRECTANGULAR' });
     }
     
     document.getElementById('result_screen').style.visibility = 'collapse'
     document.getElementById('start_button').style.visibility = 'collapse'
     document.getElementById('pano').style.visibility = 'visible'
     
-    const panorama = new PANOLENS.ImagePanorama(`..//data/${id}.jpg`);
-    
     console.log(viewer)
-    viewer.add(panorama)
-    viewer.setPanorama(panorama);
+    viewer.setImage(`..//data/${id}.jpg`);
     console.log(viewer)
-    last_pano = panorama;
 }
